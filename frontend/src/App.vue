7e0a36b0d86dd577192650f84bb89e8f6f58eddc
Raoul Biga <samuelbiga10@gmail.com>
2026-07-27 16:32:09 +0100

    fix: suppression du try non fermé dans App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -747,8 +747,7 @@ onMounted(async () => {
 
   // -----------------------------
 
-  try {
-    lenis = new Lenis({
+  lenis = new Lenis({
     duration: 1.2,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     smooth: true,
