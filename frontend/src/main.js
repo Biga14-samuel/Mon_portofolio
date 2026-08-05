b2a2bbfed94703adbce20a6def70da8ef1087333
Raoul Biga <samuelbiga10@gmail.com>
2026-08-05 15:47:57 +0100

    feat: ajout de vercel analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
+import { inject } from '@vercel/analytics';
 import App from './App.vue';
 import './assets/styles.css';
 import magnetic from './directives/magnetic';
+
+// Inject Vercel Analytics
+inject();
 
 const app = createApp(App);
 app.directive('magnetic', magnetic);
